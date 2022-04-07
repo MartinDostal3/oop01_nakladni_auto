8fef54739ea4afc257403bf7a527e1468e86c91a
Dusty <martin.70cz@gmail.com>
2022-04-07 20:57:17 +0200

    Přidejte soubory projektu.

---
+++ oop01/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oop01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NakladniAuto nakladak = new NakladniAuto("ABC123", 3);
+
+            double zbytek = nakladak.NalozNaklad(4);
+            if (zbytek > 0)
+            {
+                MessageBox.Show("Nebylo naloženo: " + zbytek + " t");
+            }
+            MessageBox.Show( nakladak.ToString());
+
+            double chybi = nakladak.VylozNaklad(7);
+            if (chybi > 0)
+            {
+                MessageBox.Show("Chybí materiálu: " + chybi + " t");
+            }
+            MessageBox.Show(nakladak.ToString());
+
+        }
+    }
+}
---
+++ oop01/NakladniAuto.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop01
+{
+    
+    internal class NakladniAuto
+    {
+        private string spz;
+        private double nosnost;
+        private double HmotnostNakladu = 0;
+
+        public NakladniAuto(string spz, double nosnost)
+        {
+            this.spz = spz;
+            this.nosnost = nosnost;
+        }
+        public double GetHmotnostNakladu(double HmotnostNakladu)
+        {
+            return HmotnostNakladu;
+        }
+
+
+        public string GetSpz(string spz)
+        {
+            return spz;
+        }
+        
+
+        public double GetNosnost(double nosnost)
+        {
+            return nosnost;
+        }
+
+        public double NalozNaklad(double hmotnostNakladky)
+        {
+            double nebyloNalozeno = 0;
+           if((HmotnostNakladu + hmotnostNakladky) <= nosnost)
+            {
+                HmotnostNakladu += hmotnostNakladky;
+            }
+            else
+            {
+                nebyloNalozeno = HmotnostNakladu + hmotnostNakladky - nosnost;
+                 HmotnostNakladu = nosnost;
+                //HmotnostNakladu += hmotnostNakladky - nebyloNalozeno;
+               
+            }
+            return nebyloNalozeno;
+         
+
+            
+        }
+        public double VylozNaklad(double hmotnostVykladky)
+        {
+            double chybiMaterialu = 0;
+            if(hmotnostVykladky > HmotnostNakladu)
+            {
+                chybiMaterialu = hmotnostVykladky - HmotnostNakladu; ;
+                HmotnostNakladu = 0;
+            }
+            else 
+            {
+                HmotnostNakladu -= hmotnostVykladky;
+            }
+
+            return chybiMaterialu;
+
+        }
+        public override string ToString()
+        {
+
+            string s = "\nNákladní auto: " + spz
+                + "\nMá nosnost: " + nosnost + " t"
+                + "\nMá naloženo: " + HmotnostNakladu + " t";
+
+
+
+            return base.ToString() + s;
+        }
+
+
+
+
+
+    }
+}
